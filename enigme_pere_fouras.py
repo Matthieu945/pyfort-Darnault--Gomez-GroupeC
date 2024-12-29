ec0d5ccc318e3df4c37ae4eee6ce94e4482f5d42
matthieudarnault <matthieu.darnault@efrei.net>
2024-12-29 18:22:12 +0100

    fin enigme_pere_fouras

--- enigme_pere_fouras.py
+++ enigme_pere_fouras.py
@@ -10,21 +10,27 @@ import random
 
 def enigme_pere_fouras(enigmes) :
     liste_enigme = []
+    liste_reponse = []
     for enigme in enigmes :
         if "question" in enigme :
             liste_enigme.append(enigme["question"])
-    enigme_aleatoire = random.choice(liste_enigme)
+            liste_reponse.append(enigme["reponse"])
+    numero_aleatoire = random.randint(0, len(liste_enigme))
+    enigme_aleatoire = liste_enigme[numero_aleatoire]
+    reponse_attendue = liste_reponse[numero_aleatoire]
     print(enigme_aleatoire)
-    print("Vous avez le droit à trois essais.")
     essai = 3
-    resultat_joueur = input("Votre réponse :")
-    while essai > 0 :
-        if resultat_joueur.lower() == enigme_aleatoire["reponse"].lower() :
-            print("Bonne réponse")
+    print("Vous avez le droit à trois essais.")
+    while essai > 1 :
+        resultat_joueur = input("Votre réponse :")
+        if resultat_joueur.lower() == reponse_attendue.lower() :
+            print("Bonne réponse ! Vous gagnez une clé.")
             return True
         else :
-            print("Mauvaise réponse")
-            essai -=1
+            print("Mauvaise réponse. Il vous reste encore", essai - 1, "essai(s).")
+            essai = essai - 1
+    if essai == 0 :
+        print(f"Vous avez perdu. La solution était {reponse_attendue.lower()}. Vous ne gagnez pas de clé.")
     return False
 
 enigmes = charger_enigmes(fichier)
