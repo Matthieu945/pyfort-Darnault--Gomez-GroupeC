f45a1317ce249e3ae4bc9162833ed43d2494dc87
matthieudarnault <matthieu.darnault@efrei.net>
2025-01-04 22:23:50 +0100

    fin fonctions utiles

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,5 +1,3 @@
-from unicodedata import normalize
-
 
 def introduction():
     print("Bonjour à vous, chers joueurs et joueuses !\n"
@@ -8,7 +6,8 @@ def introduction():
         "afin de récolter un objectif de trois clés pour accéder à la salle du trésor !\n"
         "Vous allez maintenant composer votre équipe.\n"
         "\n")
-    input("Appuyer sur 'Entrée' pour composer votre équipe.")
+    input("Appuyer sur 'Entrée' pour composer votre équipe.\n"
+          "\n")
 
 def composer_equipe() :
     nombre_joueurs = int(input("Veuillez saisir le nombre de joueurs dans votre équipe : "))
@@ -18,6 +17,7 @@ def composer_equipe() :
     equipe = []
     cpt_leader = 0
     for i in range(nombre_joueurs) :
+        print("\n")
         print(f"Saisie du joueur {i + 1} :")
         nom = input("Nom du joueur : ")
         profession = input("Profession : ")
@@ -44,7 +44,9 @@ def composer_equipe() :
         print("Aucun joueur n'a été sélectionné comme Leader. Le joueur 1 a donc été sélectionné automatiquement comme Leader.")
         equipe[0]["leader"] = "Leader"
 
+    print("\n")
     print("Voici votre équipe :")
+    print("\n")
     for i in range(len(equipe)) :
         print(f"Joueur {i + 1} :", end="     ")
         print(f"Nom : {equipe[i]['nom']}")
@@ -52,8 +54,33 @@ def composer_equipe() :
         print(f"               Rôle : {equipe[i]['leader']}")
         print()
 
+    return equipe
+
+
+def menu_epreuve() :
+    print("Choisissez votre epreuve en saisissant le numéro correspondant.")
+    epreuves = {1 : "Épreuve de Mathématiques", 2 : "Épreuve de logique", 3 : "Épreuve de hasard", 4 : "Énigme du Père Fouras"}
+    for numero, nom in epreuves.items():
+        print(f"{numero}. {nom}")
+    choix = 0
+    while choix < 1 or choix > 4:
+        choix = int(input("Choix : "))
+    return choix
 
 
 
+def choisir_joueur(equipe):
+    print("Sélectionnez un joueur de l'équipe pour participer à cette épreuve.")
+    i = 1
 
-composer_equipe()
+    for joueur in equipe:
+        print(f"{i}. {joueur['nom']} ({joueur['profession']}) - {joueur['leader']}")
+        i += 1
+
+    joueur_choisi = int(input(("Entrez le numéro du joueur : ")))
+
+    return equipe[joueur_choisi - 1]
+
+
+
+equipe = composer_equipe()
